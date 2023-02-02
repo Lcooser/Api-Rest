c0b9315941f6be8f3ddbd227f5cd8db91a12bfcb
Luiz Coser <luiz.cooser1@gmail.com>
2023-02-02 00:11:02 -0300

    Adicionando método REMOVE

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -47,10 +47,24 @@ async function put(req, res){
 
 }
 
+async function remove(req, res){
+
+    const { id } =  req.params
+
+    const remove = await ProductsModel.deleteOne({_id: id})
+
+    const message = remove.ok ? 'error' : 'success'
+
+    res.send({
+        message,
+    })
+}
+
+
 module.exports = {
     get,
     post,
     put,
-
+    remove,
 
 }
